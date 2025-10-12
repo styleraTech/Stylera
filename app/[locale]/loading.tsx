da5aadf0d755a5fe6b81b19ec6f1c87c95959653
saje0 <saje.dev963@gmail.com>
2025-10-12 19:39:36 +0300

    fix loading page spacing

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -3,7 +3,7 @@ import LoadingSpinner from '@/components/loading-spinner'
 export default function Loading() {
   return (
     <div className='min-h-screen bg-background flex items-center justify-center'>
-      <div className='text-center mx-auto'>
+      <div className='text-center flex justify-center items-center flex-col'>
         <LoadingSpinner size='lg' />
         <p className='text-slate-400 mt-4'>Loading...</p>
       </div>
