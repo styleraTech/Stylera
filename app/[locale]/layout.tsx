af66cecac3ac894964be7a149bd6778f7e2ae5b8
saje0 <saje.dev963@gmail.com>
2025-10-13 18:37:26 +0300

    font changed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
-import Navbar from '@/components/layout/navigation'
 import Footer from '@/components/layout/footer'
-import { Figtree, Cairo } from 'next/font/google'
+import { Cairo } from 'next/font/google'
 import { Toaster } from 'sonner'
 import './globals.css'
 
@@ -12,31 +11,20 @@ export const generateStaticParams = async () => {
 import localFont from 'next/font/local'
 import { getDictionary } from '@/get-dictionary'
 
+// English font (local)
 const nasalization = localFont({
   src: '../../public/fonts/FontsFree-Net-nasalization-rg.ttf',
   variable: '--font-english',
   display: 'swap',
 })
 
-const geDinar = localFont({
-  src: '../../public/fonts/GE-Dinar-One-Medium.otf',
+// Arabic font (Google Cairo)
+const cairo = Cairo({
+  subsets: ['arabic'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-arabic',
   display: 'swap',
 })
-
-// const figtree = Figtree({
-//   subsets: ['latin'],
-//   weight: ['300', '400', '500', '600', '700'],
-//   variable: '--font-figtree',
-//   display: 'swap',
-// })
-
-// // Arabic Font
-// const cairo = Cairo({
-//   subsets: ['arabic'],
-//   weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
-//   display: 'swap',
-// })
 
 export default async function LocaleLayout({
   children,
@@ -51,13 +39,14 @@ export default async function LocaleLayout({
   return (
     <div
       className={`${
-        locale === 'ar' ? geDinar.className : nasalization.className
+        locale === 'ar' ? cairo.className : nasalization.className
       } antialiased`}
       dir={locale === 'ar' ? 'rtl' : 'ltr'}
       lang={locale}
     >
       {children}
       <Footer dictionary={dictionary.footer} />
+      <Toaster />
     </div>
   )
 }
