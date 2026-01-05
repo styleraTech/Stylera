0c2de9cc921707fc375ea733c6f674c1ce7b20c1
LastHolmz <databaserepos@gmail.com>
2026-01-05 10:54:54 +0200

    fix images

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -14,8 +14,8 @@ export const ourFileRouter = {
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
-      maxFileSize: "4MB",
-      maxFileCount: 1,
+      maxFileSize: "1024MB",
+      maxFileCount: 10,
     },
   })
     // Set permissions and file types for this FileRoute
